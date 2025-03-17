819fd15badbdd4cc304ed3d189d6b49116518c8d
Juan Fahrul Fauzan <juanfhrul08@gmail.com>
2025-03-17 10:57:07 +0700

    adj:change network type to Ethernet 'changable'

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@ import os from 'os';
 
 export function getIPAddress() {
     const networkIP = os.networkInterfaces();
-    const { address } = networkIP["Wi-Fi"].filter((content) => content.family === "IPv4")[0];
+    const { address } = networkIP["Ethernet 2"].filter((content) => content.family === "IPv4")[0];
 
     return address;
 }
