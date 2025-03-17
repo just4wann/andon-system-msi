2e7d07f1a498d5e1a49b20f10402f8adbc6d01d6
Juan Fahrul Fauzan <juanfhrul08@gmail.com>
2025-03-17 10:52:23 +0700

    adj:more less code and readable

--- src/main.js
+++ src/main.js
@@ -1,32 +1,8 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from "url";
+import { Server } from './server/index.js';
 
-import { Router } from './routes/router.js';
-import { TCPGateway } from './services/TCPGateway/index.js';
-import { WebsocketGateway } from './services/WebsocketGateway/index.js';
-import { getIPAddress } from './utils/index.js';
+const server = new Server();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
-
-const port = 3000;
-const ip = getIPAddress();
-const router = new Router(app);
-const websocket = new WebsocketGateway(8001);
-const tcp = new TCPGateway(websocket);
-
-websocket.createServer();
-tcp.connect(8000);
-router.setupRouter();
-
-app.listen(port, ip, () => {
-    console.log(`server listen on ${ip}:${port}`);
-})
+server.setCorsConfig();
+server.setSSRWebConfig();
+server.serviceOpen();
+server.serverStart();
